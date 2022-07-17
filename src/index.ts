5adbec1fe92794fb7fa7b98dcdd1ebbe439c2e21
Rodrigo Santos <rodrigo.v.santos89@gmail.com>
2022-07-17 11:08:47 -0300

    debiguei e refatorei

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import BandBusiness from "./business/BandBusiness";
-import PostBusiness from "./business/PostBusiness";
 import ShowBusiness from "./business/ShowBusiness";
 import { UserBusiness } from "./business/UserBusiness";
 import { app } from "./controller/app";
 import BandController from "./controller/BandController";
-import PostController from "./controller/PostController";
 import ShowController from "./controller/ShowController";
 import UserController from "./controller/UserController";
 import BandData from "./data/BandData";
-import PostData from "./data/PostData";
 import ShowData from "./data/ShowData";
 import UserData from "./data/UserData";
 import { Authenticator } from "./services/Authenticator";
@@ -23,43 +20,23 @@ const userBusiness = new UserBusiness(
 );
 const userController = new UserController(userBusiness);
 
-
 const showBusiness = new ShowBusiness(
   new ShowData(),
   new Authenticator(),
   new IdGenerator()
 );
-const showController = new ShowController(showBusiness)
-app.post("/shows", showController.addShow)
-app.get("/shows",showController.getShowByDate)
+const showController = new ShowController(showBusiness);
 
-
-const postBusiness = new PostBusiness(
-  new PostData(),
-  new IdGenerator(),
-  new Authenticator()
-);
-const postController = new PostController(postBusiness);
-
-app.post("/user/signup", userController.signup);
-app.post("/user/login", userController.login);
-app.post("/post/create", postController.postCreator);
-app.get("/post/list", postController.postList);
-
-
-app.get("/post/:id", postController.postById);
-
-
-//criação da banda -- início
 const bandBusiness = new BandBusiness(
   new BandData(),
   new IdGenerator(),
   new Authenticator()
-)
-const bandController = new BandController(bandBusiness)
-app.post("/band", bandController.addBand)
-// -- fim --
+);
+const bandController = new BandController(bandBusiness);
 
-// pegar uma banda por id ou nome
-app.get("/banda/:id", bandController.getBand)
-
+app.post("/user/signup", userController.signup);
+app.post("/user/login", userController.login);
+app.post("/shows", showController.addShow);
+app.post("/band", bandController.addBand);
+app.get("/shows", showController.getShowByDate);
+app.get("/banda/:id", bandController.getBand);
